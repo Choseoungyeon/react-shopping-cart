e3c24d6383234b98295513f92e15bbd3d57d7347
Cho <bottle34@naver.com>
2022-05-02 23:38:13 +0900

    2022 05 02

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 //Components
 import Item from "./Item/Item";
+import Cart from "./Cart/Cart";
 import { Drawer, LinearProgress, Grid, Badge } from "@mui/material";
 import { AddShoppingCart } from "@mui/icons-material";
 //Styles
@@ -33,7 +34,22 @@ const App = () =>{
   const getTotalItems = (items : CartItemType[])=>
     items.reduce((ack:number, item) => ack + item.amount, 0)
   ;
-  const handleAddToCart = (clickedItem : CartItemType)=>null;
+  const handleAddToCart = (clickedItem : CartItemType)=>{
+    setCartItems(prev => {
+      //1. Is the item already added in the cart?
+      const isItemInCart = prev.find(item => item.id === clickedItem.id)
+
+      if(isItemInCart) {
+        return prev.map(item => (
+          item.id === clickedItem.id
+          ?{...item, amount:item.amount + 1}
+          : item
+        ))
+      }
+      //First time the item is added
+      return[...prev, {...clickedItem, amount:1}]
+    })
+  };
   const handleRemoveFromCart = () => null
   
   if(isLoading) return <LinearProgress />
@@ -42,7 +58,7 @@ const App = () =>{
   return (
     <Wrapper>
       <Drawer anchor="right" open={cartOpen} onClose={()=>setCartOpen(false)}>
-        Cart goes here
+        <Cart cartItem={cartItems} addToCart={handleAddToCart} removeFromCart={handleRemoveFromCart} />
       </Drawer>
       <StyledButton onClick={()=>setCartOpen(true)}>
         <Badge badgeContent={getTotalItems(cartItems)} color="error">
